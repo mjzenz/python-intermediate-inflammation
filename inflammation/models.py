b1ef38a349e22f18ffa56c76e20d82ad02c259e6
MICHAEL J ZENZ <zenz@wisc.edu>
2025-03-24 14:57:51 -0500

    added pylint and fixed package code lint

--- inflammation/models.py
+++ inflammation/models.py
@@ -10,7 +10,7 @@ and each column represents a single day across all patients.
 import numpy as np
 
 
-def load_csv(filename):  
+def load_csv(filename):
     """Load a Numpy array from a CSV
 
     :param filename: Filename of CSV to load
@@ -43,4 +43,3 @@ def daily_min(data):
        :returns: An array of minimum values of measurements for each day.
        """
     return np.min(data, axis=0)
-
